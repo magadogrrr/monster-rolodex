6fcc114f89f0e8cbabc9d6f528b33a7caec7fbfb
torrence payseno <tpayseno@gmail.com>
2020-02-12 19:19:20 -0500

    Updates

---
+++ precache-manifest.f9c8bafee5007f4c597ce921752962f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd297f39a8e4d01d93fd3e931ad6720a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4de08831ec7ae6e0abf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04f5747e520d0ed63807",
+    "url": "/static/js/2.fdd34e15.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fdd34e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4de08831ec7ae6e0abf",
+    "url": "/static/js/main.f433bc06.chunk.js"
+  },
+  {
+    "revision": "863018210bf1c6ae9c68",
+    "url": "/static/js/runtime-main.215bd54d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
